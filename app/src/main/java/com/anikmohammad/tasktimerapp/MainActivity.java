b4a5daacdf35660444cc88e8c92b7395d297aeb0
Anik Mohammad <anik7303@gmail.com>
2019-10-28 01:03:50 +0600

    fixing import (adding appcompat libraries)

--- app/src/main/java/com/anikmohammad/tasktimerapp/MainActivity.java
+++ app/src/main/java/com/anikmohammad/tasktimerapp/MainActivity.java
@@ -1,7 +1,6 @@
 package com.anikmohammad.tasktimerapp;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -15,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
